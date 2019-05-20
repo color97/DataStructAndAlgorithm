13fd6feeeb06e93e5b1cc812a449296b519b87df
color97 <807065797@qq.com>
2019-05-21 01:13:58 +0800

    selection problem

--- src/1.programming_a_general_overview/1.selection_problem.cpp
+++ src/1.programming_a_general_overview/1.selection_problem.cpp
@@ -1,4 +1,8 @@
 #include <vector>
+//#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -7,41 +11,58 @@ class Solution
 public:
     int selection_problem(const vector<int>& numbers, const int k)
     {
-        vector<int> tmpNumbers(k);
+        vector<int> kNumbers(k);
         for(int i = 0; i < k; i++)
         {
-            tmpNumbers[i] = numbers[i];
+            kNumbers[i] = numbers[i];
         }
         for (size_t i = 0; i < k; i++)
         {
             for(size_t j = 0; j < k - i - 1; j++)
             {
-                if(tmpNumbers[j] < tmpNumbers[j+1])
+                if(kNumbers[j] < kNumbers[j+1])
                 {
-                    std::swap(tmpNumbers[j] < tmpNumbers[j+1]);
+                    std::swap(kNumbers[j], kNumbers[j+1]);
                 }
             }
         }
         for (size_t i = k; i < numbers.size(); i++)
         {
-            if(tmpNumbers[k-1] < numbers[i])
+            if(kNumbers[k-1] < numbers[i])
             {
-                for (size_t j = 0; j < k; j++)
+                for (vector<int>::iterator itr = kNumbers.begin(); itr < kNumbers.begin() +k; itr++)
                 {
-                    if(numbers[i] > tmpNumbers[j])
+                    if(numbers[i] > *itr)
                     {
-                        tmpNumbers.insert(j, numbers[i]);
+                        kNumbers.insert(itr, numbers[i]);
+                        break;
                     }
                 }
                 
             }
         }
-        
+        return kNumbers[k - 1];
         
     }
 };
 
 int main()
 {
-
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+    const vector<int> Narray{10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+    for(auto & N: Narray)
+    {
+        const int k = N / 2;
+        vector<int> numbers(N);
+        for( auto& number : numbers)
+        {
+            number = rand();
+        }
+        Solution solution;
+        clock_t timeBegin = clock();
+        const int kLargest = solution.selection_problem(numbers, k);
+        clock_t timeEnd = clock();
+        printf("N = %10d, cost time = %10.2fms, the %10dth largest is %d.\n", N, 1000.0 * (timeEnd - timeBegin)/CLOCKS_PER_SEC, k, kLargest);
+        //cout<<"N = "<< N <<"cost time = "<<<<", the "<< k <<"th largest is "<< kLargest << ".\n";
+    }
 }
