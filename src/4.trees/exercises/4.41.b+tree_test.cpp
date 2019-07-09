3a779a9d192dc05007bd0d8b938ff8d5ec1c33c5
color <wujiale0907@qq.com>
2019-07-10 01:16:44 +0800

    add 4.41 test

---
+++ src/4.trees/exercises/4.41.b+tree_test.cpp
@@ -0,0 +1,22 @@
+#include "src/4.trees/exercises/4.41.b+tree.h"
+
+int main()
+{
+    BPlusTree<int> btree;
+    btree.insert(0);
+    btree.insert(1);
+    btree.insert(2);
+    btree.insert(3);
+    btree.insert(4);
+    btree.insert(5);
+    btree.insert(6);
+    btree.insert(7);
+    btree.insert(8);
+    btree.insert(9);
+    btree.insert(10);
+    btree.insert(11);
+    btree.insert(12);
+
+
+    return 0;
+}
