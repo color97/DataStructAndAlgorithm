888161affbed51975b6cd965514f2dc395d1e963
color97 <807065797@qq.com>
2019-07-26 01:49:35 +0800

    exercises 7

---
+++ src/7.sorting/codes/7.7.6.quick_select.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+const T& median3(vector<T>& a, int left, int right)
+{
+    const int center = (left + right) / 2;
+    if (a[center] < a[left])
+    {
+        std::swap(a[left], a[center]);
+    }
+    if (a[right] < a[left])
+    {
+        std::swap(a[left], a[right]);
+    }
+    if (a[right] < a[center])
+    {
+        std::swap(a[center], a[right]);
+    }
+    
+    std::swap(a[center], a[right - 1]);
+    return a[right - 1];
+}
+
+template <typename T>
+void insertionSort(vector<T>& a, int left, int right)
+{
+    for( int p = 1; p < a.size(); p++)
+    {
+        T tmp = std::move(a[p]);
+        int i = p;
+        for(; i > 0 && tmp < a[i - 1]; i--)
+        {
+            a[i] = std::move(a[i - 1]);
+        }
+        a[i] = std::move(tmp);
+    }
+}
+
+template <typename T>
+void quickSelect(vector<T>& a, int left, int right, int k)
+{
+    if (left + 10 <= right)
+    {
+        const T& pivot = median3(a, left, right);
+
+        int i = left, j = right - 1;
+        for( ; ; )
+        {
+            while(a[++i] < pivot) { }
+            while(pivot < a[--j]) { }
+            if (i < j)
+            {
+                std::swap(a[i], a[j]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        std::swap(a[i], a[right - 1]);
+        if (k < i)
+        {
+            quickSelect(a, left, i - 1);
+        }
+        else (i < k)
+        {
+            quickSelect(a, i + 1, right);            
+        }
+    }
+    else
+    {
+        insertionSort(a, left, right);
+    }
+}
+
+
+template <typename T>
+void quickSelect(vector<T>& a, int k)
+{
+    quickSelect(a, 0, a.size() - 1, int k);
+}
--- src/7.sorting/exercises/amswer.md
+++ src/7.sorting/exercises/amswer.md
@@ -13,3 +13,26 @@ step8:      1, 1, 2, 3, 4, 5, 5, 6, 9
 a. Θ(N^2), 定理7.3 证明
 
 9. 
+a. O(NlogN), 每趟 是 N / h 的 h次插入排序，因为有序，花费 h * N / h = N, 而共有 logN 趟这样的排序（希尔增量）
+b. O(NlogN) ？
+
+13. Heapsort uses at least (roughly) N log N comparisons on any input, so there are no particularly good
+inputs. This bound is tight; see the paper by Schaeffer and Sedgewick [18]. This result applies for
+almost all variations of heapsort, which have different rearrangement strategies. See Y. Ding and M.
+A. Weiss, “Best Case Lower Bounds for Heapsort,” Computing 49 (1992)
+
+17. Θ(N log N) ，因为归并操作 Θ(N)
+
+20. 
+a. O(NlogN) ,pivot 处于中间
+b. O(NlogN) ,pivot 处于中间
+c. O(NlogN) , 三数中值分割法和截至技术。快速排序平均 O(NlogN)
+
+21. 
+a. O(N^2), O(N^2), O(NlogN)
+b. O(N^2), O(N^2), O(NlogN)
+c. O(NlogN), O(NlogN), O(NlogN)
+although there is an O(N2) worst case if very bad random numbers come up
+d. 一般来说都是 O(NlogN)。
+但是当序列为: 1, 2, 4, ..., 2^(k-1)时，平均值造成不均匀的分组。O(N^2)
+
